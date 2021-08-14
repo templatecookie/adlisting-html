bb5be5319c61b4be04e76547a42a979f6e78ca08
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-14 22:03:34 +0600

    support brand slider section created

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -32,3 +32,38 @@ $('.testimonial-slider').slick({
     },
   ],
 });
+
+$('.support-brand__slider').slick({
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 1200,
+  centerMode: true,
+  centerPadding: '0px',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
