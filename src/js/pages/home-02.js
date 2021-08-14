cf78a48109da5ccfcf392bf22b297567dfd77a86
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-14 18:05:18 +0600

    added header layout in getmembership page

--- src/js/pages/home-02.js
+++ src/js/pages/home-02.js
@@ -1,16 +1,1 @@
 'use strict';
-
-// Header Navigation Toggle event
-const toggle = document.querySelector('.toggle-icon');
-const mainNav = document.querySelector('.header');
-const mobileNav = document.querySelector('.menu--sm');
-const logo = document.querySelector('[alt="brand-logo"]');
-
-let logoChange = false;
-
-toggle.addEventListener('click', function (e) {
-  e.preventDefault();
-  toggle.classList.toggle('active');
-  mainNav.classList.toggle('active');
-  mobileNav.classList.toggle('active');
-});
