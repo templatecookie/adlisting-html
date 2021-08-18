d56322c4294621c8c0a5a63fdc3bce939d92e3fc
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-18 14:57:55 +0600

    added pagination , responsive checked , w3 validation checked

--- src/js/pages/blog.js
+++ src/js/pages/blog.js
@@ -2,14 +2,39 @@
 
 $(document).ready(function () {
   $('.galleryView').venobox();
+  $('.yplayer').venobox({
+    spinner: 'wave',
+    spinColor: '#00aaff',
+  });
 });
 
 const blogSidebar = document.querySelector('.blog__sidebar');
-const toggle = document.querySelector('.blog__sidebar .toggle-icon');
+const toggleBtn = document.querySelector('.blog__sidebar .toggle-icon');
 let body = document.querySelector('body');
 
-toggle.addEventListener('click', function (e) {
+toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   blogSidebar.classList.toggle('active');
   body.classList.toggle('overlay');
+});
+
+// Header Navigation Toggle event
+const toggle = document.querySelector('.toggle-icon');
+const mainNav = document.querySelector('.header');
+const mobileNav = document.querySelector('.menu--sm');
+const logo = document.querySelector('[alt="brand-logo"]');
+
+let logoChange = false;
+
+toggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  toggle.classList.toggle('active');
+  mainNav.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+
+  // change logo if it's background are transparent
+  // logoChange === true
+  //   ? (logo.src = 'src/images/logo.png')
+  //   : (logo.src = 'src/images/logo-dark.png');
+  // logoChange = !logoChange;
 });
