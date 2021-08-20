98846fee8805215a6a34b1a65d87391b7696d0bd
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-20 16:11:40 +0600

    added popup in about page

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -67,3 +67,11 @@ $('.support-brand__slider').slick({
     },
   ],
 });
+
+$(document).ready(function () {
+  // $('.galleryView').venobox();
+  $('.yplayer').venobox({
+    spinner: 'wave',
+    spinColor: '#00aaff',
+  });
+});
