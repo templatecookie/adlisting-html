717adc1a4f717bf7d98bb0626eae870142d2d374
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-21 21:23:31 +0600

    product ad slider added

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // 'use strict';
+// import css files
+import './src/js/plugins/css/slick.css';
+import './src/js/plugins/css/venobox.min.css';
+
 // import main scss file
 import 'bootstrap/scss/bootstrap.scss';
 import './src/scss/main.scss';
