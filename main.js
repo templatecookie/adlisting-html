8e449f6427b6b1d5d0d660a3eadaf543032d0cc7
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-22 18:23:01 +0600

    prices range slider added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // import css files
 import './src/js/plugins/css/slick.css';
 import './src/js/plugins/css/venobox.min.css';
+import './src/js/plugins/css/nouislider.min.css';
 
 // import main scss file
 import 'bootstrap/scss/bootstrap.scss';
