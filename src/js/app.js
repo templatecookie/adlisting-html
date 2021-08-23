5cf7155b38de274e2df335a99352767cae0375ea
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-23 07:42:07 +0600

    homepage 01 added aos animation

--- src/js/app.js
+++ src/js/app.js
@@ -28,3 +28,11 @@ function scrollToTop() {
     behavior: 'smooth',
   });
 }
+// Sticky header
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// Init scroll animation
+AOS.init({ disable: 'mobile' });
