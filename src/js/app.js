8345b8f3b12a46178f38303e3bd0dbfb0efc5764
Sheikh Rashed <mdrashed1310@gmail.com>
2021-08-23 06:25:21 +0600

    added loader

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,11 @@
 'use strict';
 
-// Header Navigation Toggle event
-// const toggle = document.querySelector('.toggle-icon');
-// const mainNav = document.querySelector('.header');
-// const mobileNav = document.querySelector('.menu--sm');
-// const logo = document.querySelector('[alt="brand-logo"]');
+//  Page Loader
+const preloader = document.querySelector('.loader');
 
-// let logoChange = false;
-
-// toggle.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   toggle.classList.toggle('active');
-//   mainNav.classList.toggle('active');
-//   mobileNav.classList.toggle('active');
-
-//   // change logo if it's background are transparent
-//   // logoChange === true
-//   //   ? (logo.src = 'src/images/logo.png')
-//   //   : (logo.src = 'src/images/logo-dark.png');
-//   // logoChange = !logoChange;
-// });
+window.addEventListener('load', (event) => {
+  preloader.style.display = 'none';
+});
 
 // Cards Edit menu
 const editBtn = document.querySelectorAll('.edit-icon');
