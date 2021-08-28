1dcaa8bd52a50aefee05c51a585ffb2a3986c997
Zakir Hossen <web.zakirbd@gmail.com>
2021-08-28 09:28:32 +0600

    fix: vendor files move to main scss file

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 // 'use strict';
 // import css files
-import './src/js/plugins/css/slick.css';
-import './src/js/plugins/css/venobox.min.css';
-import './src/js/plugins/css/nouislider.min.css';
-import 'animate.css';
-
-// import main scss file
-import 'bootstrap/scss/bootstrap.scss';
 import './src/scss/main.scss';
