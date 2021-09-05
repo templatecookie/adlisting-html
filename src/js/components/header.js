6128e458cb5cb40a2c7f25e2e9f8219cd1fe9446
SheikhRashed <devrashed06@gmail.com>
2021-09-05 19:21:22 +0600

    added header components

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Header Navigation Toggle event
 const toggle = document.querySelector('.toggle-icon');
 const mainNav = document.querySelector('.header');
@@ -15,8 +13,8 @@ toggle.addEventListener('click', function (e) {
   mobileNav.classList.toggle('active');
 
   // change logo if it's background are transparent
-  // logoChange === true
-  //   ? (logo.src = 'src/images/logo.png')
-  //   : (logo.src = 'src/images/logo-dark.png');
-  // logoChange = !logoChange;
+  logoChange === true
+    ? (logo.src = 'src/images/logo.png')
+    : (logo.src = 'src/images/logo-dark.png');
+  logoChange = !logoChange;
 });
--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,19 +1,5 @@
 'use strict';
 
-const toggle = document.querySelector('.toggle-icon');
-const mainNav = document.querySelector('.header');
-const mobileNav = document.querySelector('.menu--sm');
-const logo = document.querySelector('[alt="brand-logo"]');
-
-let logoChange = false;
-
-toggle.addEventListener('click', function (e) {
-  e.preventDefault();
-  toggle.classList.toggle('active');
-  mainNav.classList.toggle('active');
-  mobileNav.classList.toggle('active');
-});
-
 $('.testimonial-slider').slick({
   infinite: true,
   slidesToShow: 2,
