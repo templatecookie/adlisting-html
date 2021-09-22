fa803b1a962471b8fb86f9558a30e99a24fe1e46
SheikhRashed <devrashed06@gmail.com>
2021-09-22 18:32:04 +0600

    removed comments

--- main.js
+++ main.js
@@ -4,20 +4,6 @@
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick.min.js';
-// import './src/js/plugins/jquery.min.js';
-// import './src/js/plugins/bvselect';
-// import './src/js/plugins/select2/css/select2-bootstrap-5-theme.css';
-// import 'animate.css';
-// // import 'venobox/venobox/venobox';
-// import 'select2/dist/css/select2.min.css';
-// import 'select2/dist/js/select2.min.js';
-// import '@srexi/purecounterjs';
-// import 'nouislider/dist/nouislider.min.css';
-
-// import 'nouislider/dist/nouislider.min.js';
-// import 'wnumb/wNumb.min.js';
 
 // import css files
 import './src/scss/main.scss';
