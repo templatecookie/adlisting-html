28b000227a2d32a917bdb2dc73fc91f3b70021a7
SheikhRashed <devrashed06@gmail.com>
2021-10-20 14:59:00 +0600

    added noui slider files

--- src/js/app.js
+++ src/js/app.js
@@ -713,6 +713,45 @@ if (sidebarToggle) {
   });
 }
 
+// Slider
+const sliderRange = document.querySelector('.sliderrange');
+const output = document.querySelector('#value-range');
+
+output.innerHTML = `${sliderRange.value} miles`;
+
+sliderRange.oninput = function () {
+  output.innerHTML = `${this.value} miles`;
+};
+
+sliderRange.addEventListener('mousemove', function () {
+  var x = sliderRange.value;
+
+  var color = `linear-gradient(
+    90deg,
+    rgb(0, 170, 255) ${x}%,
+    rgb(218, 221, 229) ${x}%
+  )`;
+
+  sliderRange.style.background = color;
+});
+
+var range = document.getElementById('priceRangeSlider');
+
+noUiSlider.create(range, {
+  start: [20, 80],
+  connect: true,
+  range: {
+    min: [0],
+    max: [500],
+  },
+  format: wNumb({
+    decimals: 3,
+    thousand: '.',
+    suffix: ' ($)',
+  }),
+  tooltips: true,
+});
+
 // if (removeBtn) {
 //   for (var i = 0; i < removeBtn.length; i++) {
 //     console.log(i);
