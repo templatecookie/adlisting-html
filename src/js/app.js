360d3646e227075a50925bf364f3f496bb5037db
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-03-22 17:06:19 +0600

    add: range slider

--- src/js/app.js
+++ src/js/app.js
@@ -837,9 +837,60 @@ window.addEventListener("scroll", function () {
 })
 
 
+// new js add
 $(".toggle-bar").on("click", function () {
     $(".list-sidebar, .offcanvas-overlay").addClass("active");
 });
 $(".close, .offcanvas-overlay").on("click", function () {
     $(".list-sidebar, .offcanvas-overlay").removeClass("active");
 });
+
+
+//range slider
+if ($('.js-range-slider').length > 0) {
+	var $range = $(".js-range-slider"),
+		$from = $(".from"),
+		$to = $(".to"),
+		range,
+		min = $range.data('min'),
+		max = $range.data('max'),
+		from,
+		to;
+
+	var updateValues = function () {
+		$from.prop("value", from);
+		$to.prop("value", to);
+	};
+
+	$range.ionRangeSlider({
+		onChange: function (data) {
+			from = data.from;
+			to = data.to;
+			updateValues();
+		}
+	});
+}
+
+// google map
+google.maps.event.addDomListener(window, 'load', init);
+        
+    function init() {
+        var mapOptions = {
+            zoom: 11,
+            center: new google.maps.LatLng(40.6700, -73.9400), // New York
+            styles: [{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.business","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.bus","elementType":"all","stylers":[{"visibility":"on"},{"saturation":"21"},{"weight":"4.05"}]}]
+        };
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(40.6700, -73.9400),
+            map: map,
+            title: 'Snazzy!'
+        });
+    }
